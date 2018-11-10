f2a15de32966cb94a5780e8111fe5f28454ed0ff
Egor <gsa74@yandex.ru>
2018-11-10 23:09:08 +0300

    cource work with psevdografics tree

---
+++ JAVA/first/src/com/human/Student.java
@@ -0,0 +1,7 @@
+package com.human;
+
+public class Student {
+    public void learn(String subject){
+        System.out.println("I`m learn " + subject);
+    }
+}
---
+++ QT/share_inheritance/cirqle.h
@@ -0,0 +1,11 @@
+#ifndef CIRQLE_H
+#define CIRQLE_H
+
+
+class Cirqle
+{
+public:
+    Cirqle();
+};
+
+#endif // CIRQLE_H
