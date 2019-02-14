f656ade2390f416fd0bc2a472826f13f33e5daaa
Egor <gsa74@yandex.ru>
2019-02-14 12:20:21 +0300

    new realization

--- backtracking/main.cpp
+++ backtracking/main.cpp
@@ -2,203 +2,199 @@
 #include <cmath>
 #include <vector>
 
-class Square{
-public:
+typedef class Square{
+private:
     int x;
     int y;
     int sideSize;    // геттеры сеттеры
 public:
+    void setX(const unsigned x){this->x = x;}
+    void setY(const unsigned y){this->y = y;}
+    void setSideSize(const unsigned sideSize){this->sideSize = sideSize;}
+    unsigned getSideSize(){return sideSize;}
+    unsigned getX(){return x;}
+    unsigned getY(){return y;}
     Square(){}
     Square(int x, int y, int sideSize){
         this->x = x;
         this->y = y;
         this->sideSize = sideSize;
     }
-};
+}Square;
 
-class Point{
+typedef class Point{
+private:
+    unsigned x;
+    unsigned y;
 public:
-    int x;
-    int y;
-public:
+    void setX(const unsigned x){this->x = x;}
+    void setY(const unsigned y){this->y = y;}
+    unsigned getX(){return x;}
+    unsigned getY(){return y;}
     Point(){}
-    Point(int x, int y){
+    Point(const int x, const int y){
         this->x = x;
         this->y = y;
     }
-    bool operator ==(class Point Point2){
+    bool operator ==(const class Point& Point2){
         return Point2.x == x && Point2.y == y;
     }
-};
+}Point;
 
-class MatrixElement{
+typedef class Row{
 public:
-    int mainSquareSide;
-    std::vector <class Point> points; // геттеры сеттеры
-    class Square square;
-    bool deleted;
+    Row(){num = 0;}
+    Square square;
+    class Row* up;
+    class Row* down;
+    class Element* elementsHEAD;
+    unsigned num;
+}Row;
+
+typedef class Column{
 public:
-    MatrixElement(class Square square, std::vector <class Point> masPoins, int mainSquareSide){
-        points = masPoins;
-        this->square = square;
-        points = masPoins;
-        this->mainSquareSide = mainSquareSide;
-    }
-};
+    Column(){num = 0;}
+    class Column* left;
+    class Column* right;
+    unsigned num;
+    Element* elementsHEAD;
+}Column;
 
-std::vector <class MatrixElement> createMatrix(int mainSquareSize){
-    std::vector <class MatrixElement> matrix;
-    for(int iteratorSquareSize = mainSquareSize - 1; iteratorSquareSize > 0; --iteratorSquareSize){
-        for(int iteratorX = 0; iteratorX < mainSquareSize; ++iteratorX){
-            for(int iteratorY = 0; iteratorY < mainSquareSize; ++iteratorY){
-                std::vector <class Point> masPoints;
-                if(iteratorX + iteratorSquareSize <= mainSquareSize &&
-                   iteratorY + iteratorSquareSize <= mainSquareSize){
-                    for(int i = iteratorY; i < iteratorY + iteratorSquareSize; ++i){
-                        for(int j = iteratorX; j < iteratorX + iteratorSquareSize; ++j){
-                            masPoints.push_back(Point(i, j));
-                        }
-                    }
-                    matrix.push_back(MatrixElement(Square(iteratorX, iteratorY, iteratorSquareSize), masPoints, mainSquareSize));
+typedef class Element{
+public:
+    class Element* up;
+    class Element* down;
+    class Element* left;
+    class Element* right;
+    Column* column;
+    Row* row;
+}Element;
+
+unsigned countRows(const unsigned mainSquareSize){
+    unsigned countRows = 0;
+    for(unsigned iteratorSize = mainSquareSize - 1; iteratorSize > 0; --iteratorSize){ // плохо
+        for(unsigned x = 0; x < mainSquareSize; ++x){
+            for(unsigned y = 0; y < mainSquareSize; ++y){
+                if(x + iteratorSize <= mainSquareSize && y + iteratorSize <= mainSquareSize){
+                    ++countRows;
                 }
             }
         }
     }
-    return matrix;
+    return countRows;
 }
 
-void printMatrix(std::vector <class MatrixElement>& matrix){
-    for(size_t i = 0; i < matrix.size(); ++i){
-        std::cout << "size = " << matrix[i].square.sideSize << " x = " << matrix[i].square.x << " y = " << matrix[i].square.y << std::endl;
-        for(size_t j = 0; j < matrix[i].points.size(); ++j){
-            std::cout << "x = " << matrix[i].points[j].x << " y = " << matrix[i].points[j].y << std::endl;
-        }
-    }
+void printRows(Row** rows){
+    Row* currentRow = rows[0];
+    do{
+        std::cout << " X = " << currentRow->square.getX()
+                  << " Y = " << currentRow->square.getY()
+                  << " sizeSide " << currentRow->square.getSideSize() << std::endl;
+        currentRow = currentRow->up;
+    }while(currentRow != rows[0]);
 }
 
-bool checkRightMatrix(std::vector <class MatrixElement> matrix){
-    if(matrix.size() == 0)
-        return true;
-    for(int y = 0; y < matrix[0].mainSquareSide; ++y){
-        for(int x = 0; x < matrix[0].mainSquareSide; ++x){
-            int countThisPoint = 0;
-            for(size_t i = 0; i < matrix.size(); ++i){
-                for(size_t j = 0; j < matrix[i].points.size(); ++j){
-                    if(matrix[i].points[j] == Point(x, y))
-                        ++countThisPoint;
+void createRows(const unsigned mainSquareSize, Row**& rows, unsigned rowsSize){
+    rows = new Row*[rowsSize];
+    for(unsigned i = 0; i < rowsSize; ++i)
+        rows[i] = new Row[rowsSize];
+    unsigned currentRow = 0;
+    for(unsigned iteratorSize = mainSquareSize - 1; iteratorSize > 0; --iteratorSize){
+        for(unsigned x = 0; x < mainSquareSize; ++x){
+            for(unsigned y = 0; y < mainSquareSize; ++y){
+                if(x + iteratorSize <= mainSquareSize && y + iteratorSize <= mainSquareSize){
+                    rows[currentRow]->num = currentRow;
+                    rows[currentRow++]->square = Square(x, y, iteratorSize);
                 }
-            }
-            if(countThisPoint > 1)
-                return false;
-        }
-    }
-    return true;
-}
-
-bool inFirst = true;
-
-void algorithmX(std::vector <class MatrixElement>& matrix,
-    std::vector <std::vector <class MatrixElement>>& solutions, std::vector <class MatrixElement> currentSolution){
-    std::vector <class MatrixElement> afterDelete;
-//    std::cout << "INFUNCTION" << std::endl;
-    size_t minSize = 999;
-    for(size_t iteratorElement = 0; iteratorElement < matrix.size(); ++iteratorElement){
-        for(size_t iteratorPoint = 0; iteratorPoint < matrix[iteratorElement].points.size(); ++iteratorPoint){
-            for(size_t i = 0; i < matrix.size(); ++i)
-                matrix[i].deleted = false;
-            for(size_t i = 0; i < afterDelete.size(); ++i)
-                            afterDelete.pop_back();
-            if(matrix[iteratorElement].points[iteratorPoint] == matrix[0].points[0]){
-                if(matrix[iteratorElement].square.sideSize < matrix[0].mainSquareSide / 2 && inFirst || matrix[iteratorElement].square.sideSize == 1){
-                    inFirst = false;
-                    return;
-                }
-                for(size_t solutionPoint = 0; solutionPoint < matrix[iteratorElement].points.size(); ++solutionPoint){
-                    for(size_t i = 0; i < matrix.size(); ++i){
-                        for(size_t j = 0; j < matrix[i].points.size(); ++j){
-                            if(matrix[iteratorElement].points[solutionPoint] == matrix[i].points[j])
-                                matrix[i].deleted = true;
-                        }
-                    }
-                }
-                for(size_t i = 0; i < matrix.size(); ++i){
-                    if(!matrix[i].deleted)
-                        afterDelete.push_back(matrix[i]);
-                }
-//                std::cout << "INSERT ELEMENT: " << matrix[iteratorElement].square.x << " "
-//                          << matrix[iteratorElement].square.y << " "
-//                          << matrix[iteratorElement].square.sideSize << std::endl;
-//                for(size_t i = 0; i < afterDelete.size(); ++i){
-//                    std::cout << afterDelete[i].square.x << " "
-//                              << afterDelete[i].square.y << " "
-//                              << afterDelete[i].square.sideSize << std::endl;
-//                }
-                if(checkRightMatrix(afterDelete)){
-                    if(minSize < afterDelete.size())
-                        continue;
-                    if(afterDelete.size() < minSize)
-                        minSize = afterDelete.size();
-                    std::vector <class MatrixElement> thisSolution;
-//                    std::cout << "RIGHT!!!!!!!!!!!!!!!" << std::endl;
-//                    std::cout << matrix[iteratorElement].square.x << " "
-//                              << matrix[iteratorElement].square.y << " "
-//                              << matrix[iteratorElement].square.sideSize << std::endl;
-                    thisSolution.push_back(matrix[iteratorElement]);
-                    for(size_t i = 0; i < currentSolution.size(); ++i){
-//                        std::cout << currentSolution[i].square.x << " "
-//                                  << currentSolution[i].square.y << " "
-//                                  << currentSolution[i].square.sideSize << std::endl;
-                        thisSolution.push_back(currentSolution[i]);
-                    }
-                    for(size_t i = 0; i < afterDelete.size(); ++i){
-//                        std::cout << afterDelete[i].square.x << " "
-//                                  << afterDelete[i].square.y << " "
-//                                  << afterDelete[i].square.sideSize << std::endl;
-                        thisSolution.push_back(afterDelete[i]);
-                    }
-                    solutions.push_back(thisSolution);
-                    continue;
-                }
-                currentSolution.push_back(matrix[iteratorElement]);
-                algorithmX(afterDelete, solutions, currentSolution);
-                currentSolution.pop_back();
             }
         }
     }
-    return;
+    for(unsigned i = 0; i < currentRow; ++i){
+        rows[i]->up = rows[i+1];
+    }
+    for(unsigned i = currentRow - 1; i > 0; --i)
+        rows[i]->down = rows[i-1];
+    rows[0]->down = rows[currentRow - 1];
+    rows[currentRow - 1]->up = rows[0];
+}
+
+void createColumns(unsigned mainSquareSize, Column**& columns){
+    columns = new Column*[static_cast<int>(pow(mainSquareSize, 2))];
+    for(unsigned i = 0; i < static_cast<unsigned>(pow(mainSquareSize, 2)); ++i){
+        columns[i] = new Column[static_cast<int>(pow(mainSquareSize, 2))];
+        columns[i]->num = i;
+    }
+    for(unsigned i = 0; i < pow(mainSquareSize, 2); ++i){
+        columns[i]->right = columns[i+1];
+    }
+    for(unsigned i = pow(mainSquareSize, 2) - 1; i > 0; --i){
+        columns[i]->left = columns[i+1];
+    }
+    columns[0]->left = columns[static_cast<int>(pow(mainSquareSize, 2)) - 1];
+}
+
+
+void insertElement(Row*& row, Column*& column){
+    Element* newElement = new Element;
+    newElement->column = column;
+    newElement->row = row;
+    std::cout << " NEW   " << " X = " << row->square.getX()
+              << " Y = " << row->square.getY()
+              << " sizeSide = " << row->square.getSideSize() << std::endl;
+    if(row->num){
+        row->num++;
+        row->elementsHEAD->right->left = newElement;
+        newElement->right = row->elementsHEAD->right;
+        row->elementsHEAD->right = newElement;
+        newElement->left = row->elementsHEAD;
+    }
+
+    if(column->num){
+        column->num++;
+        column->elementsHEAD->up->down = newElement;
+        newElement->up = column->elementsHEAD->up;
+        column->elementsHEAD->up = newElement;
+        newElement->down = column->elementsHEAD;
+    }
+
+    if(!row->num){
+        row->num++;
+        row->elementsHEAD = new Element;
+        row->elementsHEAD = newElement;
+        newElement->left = newElement;
+        newElement->right = newElement;
+    }
+    if(!column->num){
+        column->num++;
+        column->elementsHEAD = new Element;
+        column->elementsHEAD = newElement;
+        newElement->up = newElement;
+        newElement->down = newElement;
+    }
+}
+
+void createElements(Row**& rows, Column**& colums, unsigned mainSquareSize){
+    Row* currentRow = rows[0];
+    do{
+        for(unsigned y = currentRow->square.getY(); y < currentRow->square.getSideSize(); ++y){
+            for(unsigned x = currentRow->square.getX(); x < currentRow->square.getSideSize(); ++x){
+                insertElement(currentRow, colums[y * mainSquareSize + x]);
+            }
+        }
+        currentRow = currentRow->up;
+    }while(currentRow != rows[0]);
 }
 
 int main(){
-    int mainSquareSize = 0;
+    unsigned mainSquareSize = 0;
     std::cin >> mainSquareSize;
-    std::vector <class MatrixElement> matrix;
-    std::vector <std::vector <class MatrixElement>> solutions;
-    std::vector <class MatrixElement> currentSolution;
-    matrix = createMatrix(mainSquareSize);
-    algorithmX(matrix, solutions, currentSolution);
-//    for(size_t i = 0; i < solutions.size(); ++i){
-//        std::cout << "------------------" << std::endl;
-//        for(size_t j = 0; j < solutions[i].size(); ++j){
-//            std::cout << solutions[i][j].square.x << " "
-//                      << solutions[i][j].square.y << " "
-//                      << solutions[i][j].square.sideSize << std::endl;
-//        }
-//    }
-    int minSize = 999;
-    int minI = 0;
-    for(size_t i = 0; i < solutions.size(); ++i){
-        if(minSize > solutions[i].size()){
-            minI = i;
-            minSize = solutions[i].size();
-        }
-    }
-    std::cout << minSize << std::endl;
-    for(size_t i = 0; i < solutions[minI].size(); ++i){
-        std::cout << solutions[minI][i].square.x << " "
-                  << solutions[minI][i].square.y << " "
-                  << solutions[minI][i].square.sideSize << std::endl;
-    }
-
+    int rowsSize = countRows(mainSquareSize);
+    Row** rows;
+    Column** columns;
+    createRows(mainSquareSize, rows, rowsSize); // создали все возможные фигуры
+    printRows(rows);
+    createColumns(mainSquareSize, columns);
+    createElements(rows, columns, mainSquareSize);
     return 0;
 }
 
