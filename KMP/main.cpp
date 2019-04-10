984de83be2972298232a74e17458e91adaaa1532
egor <gsa74@yandex.ru>
2019-04-10 09:33:45 +0300

    better output

--- KMP/main.cpp
+++ KMP/main.cpp
@@ -2,22 +2,52 @@
 #include <string>
 #include <vector>
 
-//#define INFO
+#define INFO
 
 // calculate prefix function
 void prefixFunction(const std::string pattern_str, std::vector <int>& prefixVector) {
     size_t i = 1, j = 0;
     prefixVector[0] = 0;
     while (i != pattern_str.size()) {
+#ifdef INFO
+        std::cout << pattern_str << std::endl;
+        for(size_t k = 0; k < i; ++k)
+            std::cout << prefixVector[k];
+        std::cout << std::endl;
+        for(size_t k = 0; k < i; ++k) std::cout << " ";
+        std::cout << "|" << std::endl;
+        for(size_t k = 0; k < i; ++k) std::cout << " ";
+        std::cout << "i" << std::endl;
+        for(size_t k = 0; k < j; ++k) std::cout << " ";
+        std::cout << "|" << std::endl;
+        for(size_t k = 0; k < j; ++k) std::cout << " ";
+        std::cout << "j" << std::endl;
+#endif
         if (pattern_str[i] == pattern_str[j]) {
             prefixVector[i] = ++j;
+#ifdef INFO
+            std::cout << "elements of pattern str are equal: ++i; ++j" << std::endl;
+            std::cout << "prefix function = " << prefixVector[i] << std::endl;
+#endif
             ++i;
         }
-        else
-            if (!j)
-                prefixVector[i++] == 0;
-            else
+        else{
+#ifdef INFO
+            std::cout << "elements of pattern str are not equal" << std::endl;
+#endif
+            if (!j){
+#ifdef INFO
+                std::cout << "j == 0 : prefix function = 0" << std::endl;
+#endif
+                prefixVector[++i] == 0;
+            }
+            else{
+#ifdef INFO
+                std::cout << "j != 0 : j = prefix function(j - 1)" << std::endl;
+#endif
                 j = prefixVector[j - 1];
+            }
+        }
     }
 }
 
@@ -26,38 +56,30 @@ bool algorithmKMP(std::string& main_str, std::string& pattern_str, std::vector <
     size_t itMain = 0, itPattern = 0; // two iterator for strings
     // while don`t achive end of main string
     while (itMain != main_str.size()) {
-        if (pattern_str[itPattern] == main_str[itMain]) { // if elements equal
 #ifdef INFO
-            std::cout << "Pattent element: " << itPattern << "(" << pattern_str[itPattern] << ")" << "=="
-                << "String element: " << itMain << "(" << main_str[itMain] << ")" << std::endl;
+        std::cout << main_str << std::endl;
+        for(size_t i = 0; i < itMain; ++i) std::cout << " ";
+        std::cout << "|" << std::endl;
+        for(size_t i = 0; i < itMain - itPattern; ++i) std::cout << " ";
+        std::cout << pattern_str << std::endl;
 #endif
+        if (pattern_str[itPattern] == main_str[itMain]) { // if elements equal
             if (itPattern == pattern_str.size() - 1) { // find solution
+#ifdef INFO
+                std::cout << "finded solution!!!" << std::endl;
+#endif
                 solution.push_back(itMain - pattern_str.size() + 1);
                 hasSolution = true;
-#ifdef INFO
-                std::cout << "finded pattern in string!!!" << std::endl;
-#endif
             }
             ++itMain;     // increment
             ++itPattern;  // increment
         }
         else {
-#ifdef INFO
-            std::cout << "Pattern element: " << itPattern << "(" << pattern_str[itPattern] << ")" << "!="
-                << "String element: " << itMain << "(" << main_str[itMain] << ")" << std::endl;
-#endif
             if (!itPattern) { // if iterator pattern string == 0
                 ++itMain;
-#ifdef INFO
-                std::cout << "itPattern == 0 => ++itMain" << std::endl;
-                std::cout << "new value of itMain: " << itMain << std::endl;
-#endif
             }
             else {
                 itPattern = prefixVector[itPattern - 1]; // set new value for pattern iterator
-#ifdef  INFO
-                std::cout << "new value of itPattern: " << itPattern << std::endl;
-#endif
             }
         }
     }
